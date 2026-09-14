3fd052a04257bbdc8216b04cbc09e42d60e3c374
ishika-sancheti <ishika.sancheti02@gmail.com>
2026-09-14 17:11:56 +0530

    Created intents.py for categorisation of customer queries

---
+++ config/intents.py
@@ -0,0 +1,23 @@
+intents ={
+    "feature issues and feeback":{
+        "users request different features that they would like to use. users report feedback when a certain feature does not work how they intended."
+    },
+    "location availability requests":{
+        "users often request why spotify/certain artists are not available in their country and when they would be able to use it."
+    },
+    "issues with playback on devices":{
+        "certain buttons or features (like spotify connect, offline play) don't work/work differently on various versions of the app (web, desktop app, mobile app). users need help navigating or troubleshooting problems in these diff models."
+    },
+    "login issues":{
+        "users are not able to login and report their issues."
+    },
+    "subscription plan details and issues":{
+        "checking why they are not eligible for free premium, criterion for family plan, student plans etc."
+    },
+    "support in different languages":{
+        "people often raise queries in non-english langs which the twt acc does not support. they are redirected to the website for support in the language they have raised the query in."
+    },
+    "payment issues":{
+        "issues like card payments declined, unable to add people to the family plan after a detail is updated, payment made but premium not reflecting etc."
+    }
+}
